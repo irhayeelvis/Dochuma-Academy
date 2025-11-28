02ac0dc9a879c0ff7b6b72916ea77b59bdb14584
irhayeelvis <tony69elvis@gmail.com>
2025-11-28 10:30:55 +0100

    updated smooth javascript animations

--- assets/script.js
+++ assets/script.js
@@ -1,50 +1,307 @@
-    // Hamburger toggle
-    const ham = document.querySelector('.ham-menu');
-    const menu = document.querySelector('.off-screen-menu');
-    ham.addEventListener('click', () => {
-      ham.classList.toggle('active');
-      menu.classList.toggle('active');
-    });
-
-    document.querySelectorAll('.off-screen-menu a').forEach(link => {
-      link.addEventListener('click', () => {
-        ham.classList.remove('active');
-        menu.classList.remove('active');
-      });
-    });
-
-
-let slideIndex = 0;
-showSlides();
-
-function showSlides() {
-    const slides = document.querySelectorAll(".slide");
-    slides.forEach(slide => slide.style.display = "none");
-
-    slideIndex++;
-    if (slideIndex > slides.length) { slideIndex = 1 }
-    
-    slides[slideIndex - 1].style.display = "block";
-
-    setTimeout(showSlides, 3000);
-}
-
-  const faders = document.querySelectorAll('.fade-in');
-
-  const observer = new IntersectionObserver((entries, observer) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add('visible');
-        observer.unobserve(entry.target); // animate once
-      }
-    });
-  }, {
-    threshold: 0.2 // triggers when 20% of the element is visible
+// Hamburger toggle
+const ham = document.querySelector('.ham-menu');
+const menu = document.querySelector('.off-screen-menu');
+ham.addEventListener('click', () => {
+  ham.classList.toggle('active');
+  menu.classList.toggle('active');
+});
+document.querySelectorAll('.off-screen-menu a').forEach(link => {
+  link.addEventListener('click', () => {
+    ham.classList.remove('active');
+    menu.classList.remove('active');
   });
+});
 
-  faders.forEach(fader => {
-    observer.observe(fader);
+// fade Up
+const fadeUps = document.querySelectorAll('.header');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry, index) => {
+    if (entry.isIntersecting) {
+      const delay = index * 25; //delay value in ms
+      setTimeout(() => {
+        entry.target.classList.add('show');
+      }, delay);
+    }
   });
+}, {
+  threshold: 0.3, //trigger effect value time
+});
 
-  console.log('Heaven knows why you\'re in the console. cos there\'s no errors?');
-  console.log('Website Design and Development by Hashtag VI, follow me on X: https://x.com/@irhayeElvis and github: https://github.com/irhayeelvis');
+fadeUps.forEach((fadeUp) => {
+  observer.observe(fadeUp);
+});
+
+const fadeUps2 = document.querySelectorAll('.showcase-group-link');
+
+const observer2 = new IntersectionObserver((entries) => {
+  entries.forEach((entry, index) => {
+    if (entry.isIntersecting) {
+      const delay = index * 200; //delay value in ms
+      setTimeout(() => {
+        entry.target.classList.add('show');
+      }, delay);
+    }
+  });
+}, {
+  threshold: 0.3, //trigger effect value time
+});
+
+fadeUps2.forEach((fadeUp) => {
+  observer2.observe(fadeUp);
+});
+
+const fadeUps3 = document.querySelectorAll('.showcase-group video');
+
+const observer3 = new IntersectionObserver((entries) => {
+  entries.forEach((entry, index) => {
+    if (entry.isIntersecting) {
+      const delay = index * 200; //delay value in ms
+      setTimeout(() => {
+        entry.target.classList.add('show');
+      }, delay);
+    }
+  });
+}, {
+  threshold: 0.3, //trigger effect value time
+});
+
+fadeUps3.forEach((fadeUp) => {
+  observer3.observe(fadeUp);
+});
+
+const fadeUps4 = document.querySelectorAll('.small-container');
+
+const observer4 = new IntersectionObserver((entries) => {
+  entries.forEach((entry, index) => {
+    if (entry.isIntersecting) {
+      const delay = index * 200; //delay value in ms
+      setTimeout(() => {
+        entry.target.classList.add('show');
+      }, delay);
+    }
+  });
+}, {
+  threshold: 0.3, //trigger effect value time
+});
+
+fadeUps4.forEach((fadeUp) => {
+  observer4.observe(fadeUp);
+});
+
+const fadeUps5 = document.querySelectorAll('.container');
+
+const observer5 = new IntersectionObserver((entries) => {
+  entries.forEach((entry, index) => {
+    if (entry.isIntersecting) {
+      const delay = index * 200; //delay value in ms
+      setTimeout(() => {
+        entry.target.classList.add('show');
+      }, delay);
+    }
+  });
+}, {
+  threshold: 0.3, //trigger effect value time
+});
+
+fadeUps5.forEach((fadeUp) => {
+  observer5.observe(fadeUp);
+});
+
+const fadeUps6 = document.querySelectorAll('.grid-desktop');
+
+const observer6 = new IntersectionObserver((entries) => {
+  entries.forEach((entry, index) => {
+    if (entry.isIntersecting) {
+      const delay = index * 200; //delay value in ms
+      setTimeout(() => {
+        entry.target.classList.add('show');
+      }, delay);
+    }
+  });
+}, {
+  threshold: 0.3, //trigger effect value time
+});
+
+fadeUps6.forEach((fadeUp) => {
+  observer6.observe(fadeUp);
+});
+
+const fadeUps7 = document.querySelectorAll('.head');
+
+const observer7 = new IntersectionObserver((entries) => {
+  entries.forEach((entry, index) => {
+    if (entry.isIntersecting) {
+      const delay = index * 200; //delay value in ms
+      setTimeout(() => {
+        entry.target.classList.add('show');
+      }, delay);
+    }
+  });
+}, {
+  threshold: 0.3, //trigger effect value time
+});
+
+fadeUps7.forEach((fadeUp) => {
+  observer7.observe(fadeUp);
+});
+
+const fadeUps8 = document.querySelectorAll('.content');
+
+const observer8 = new IntersectionObserver((entries) => {
+  entries.forEach((entry, index) => {
+    if (entry.isIntersecting) {
+      const delay = index * 200; //delay value in ms
+      setTimeout(() => {
+        entry.target.classList.add('show');
+      }, delay);
+    }
+  });
+}, {
+  threshold: 0.3, //trigger effect value time
+});
+
+fadeUps8.forEach((fadeUp) => {
+  observer8.observe(fadeUp);
+});
+
+const fadeUps9 = document.querySelectorAll('.content-box');
+
+const observer9 = new IntersectionObserver((entries) => {
+  entries.forEach((entry, index) => {
+    if (entry.isIntersecting) {
+      const delay = index * 200; //delay value in ms
+      setTimeout(() => {
+        entry.target.classList.add('show');
+      }, delay);
+    }
+  });
+}, {
+  threshold: 0.3, //trigger effect value time
+});
+
+fadeUps9.forEach((fadeUp) => {
+  observer9.observe(fadeUp);
+});
+
+const fadeUps10 = document.querySelectorAll('.content-box');
+
+const observer10 = new IntersectionObserver((entries) => {
+  entries.forEach((entry, index) => {
+    if (entry.isIntersecting) {
+      const delay = index * 200; //delay value in ms
+      setTimeout(() => {
+        entry.target.classList.add('show');
+      }, delay);
+    }
+  });
+}, {
+  threshold: 0.3, //trigger effect value time
+});
+
+fadeUps10.forEach((fadeUp) => {
+  observer10.observe(fadeUp);
+});
+
+const fadeUps11 = document.querySelectorAll('.content-box');
+
+const observer11 = new IntersectionObserver((entries) => {
+  entries.forEach((entry, index) => {
+    if (entry.isIntersecting) {
+      const delay = index * 200; //delay value in ms
+      setTimeout(() => {
+        entry.target.classList.add('show');
+      }, delay);
+    }
+  });
+}, {
+  threshold: 0.3, //trigger effect value time
+});
+
+fadeUps11.forEach((fadeUp) => {
+  observer11.observe(fadeUp);
+});
+
+const fadeUps12 = document.querySelectorAll('.main');
+
+const observer12 = new IntersectionObserver((entries) => {
+  entries.forEach((entry, index) => {
+    if (entry.isIntersecting) {
+      const delay = index * 200; //delay value in ms
+      setTimeout(() => {
+        entry.target.classList.add('show');
+      }, delay);
+    }
+  });
+}, {
+  threshold: 0.3, //trigger effect value time
+});
+
+fadeUps12.forEach((fadeUp) => {
+  observer12.observe(fadeUp);
+});
+
+const fadeUps13 = document.querySelectorAll('.end');
+
+const observer13 = new IntersectionObserver((entries) => {
+  entries.forEach((entry, index) => {
+    if (entry.isIntersecting) {
+      const delay = index * 200; //delay value in ms
+      setTimeout(() => {
+        entry.target.classList.add('show');
+      }, delay);
+    }
+  });
+}, {
+  threshold: 0.3, //trigger effect value time
+});
+
+fadeUps13.forEach((fadeUp) => {
+  observer13.observe(fadeUp);
+});
+
+fadeUps12.forEach((fadeUp) => {
+  observer12.observe(fadeUp);
+});
+
+const fadeUps14 = document.querySelectorAll('.form');
+
+const observer14 = new IntersectionObserver((entries) => {
+  entries.forEach((entry, index) => {
+    if (entry.isIntersecting) {
+      const delay = index * 200; //delay value in ms
+      setTimeout(() => {
+        entry.target.classList.add('show');
+      }, delay);
+    }
+  });
+}, {
+  threshold: 0.3, //trigger effect value time
+});
+
+fadeUps14.forEach((fadeUp) => {
+  observer14.observe(fadeUp);
+});
+
+fadeUps12.forEach((fadeUp) => {
+  observer12.observe(fadeUp);
+});
+
+const fadeUps15 = document.querySelectorAll('.form img');
+
+const observer15 = new IntersectionObserver((entries) => {
+  entries.forEach((entry, index) => {
+    if (entry.isIntersecting) {
+      const delay = index * 200; //delay value in ms
+      setTimeout(() => {
+        entry.target.classList.add('show');
+      }, delay);
+    }
+  });
+}, {
+  threshold: 0.3, //trigger effect value time
+});
+
+fadeUps15.forEach((fadeUp) => {
+  observer15.observe(fadeUp);
+});
